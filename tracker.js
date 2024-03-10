6c7fa5cf90711ec7596bb7096b976381232bfe14
tomasrobino <tomaserobino@gmail.com>
2024-03-10 23:49:02 +0100

    buildAnnounceReq() progress

--- tracker.js
+++ tracker.js
@@ -68,3 +68,30 @@ function parseConRes(res) {
         connection_id: res.slice(8)
     }
 }
+
+/*
+Announce request format is the following:
+
+Offset  Size    Name    Value
+0       64-bit integer  connection_id
+8       32-bit integer  action          1 // announce
+12      32-bit integer  transaction_id
+16      20-byte string  info_hash
+36      20-byte string  peer_id
+56      64-bit integer  downloaded
+64      64-bit integer  left
+72      64-bit integer  uploaded
+80      32-bit integer  event           0 // 0: none; 1: completed; 2: started; 3: stopped
+84      32-bit integer  IP address      0 // default
+88      32-bit integer  key             ? // random
+92      32-bit integer  num_want        -1 // default
+96      16-bit integer  port            ? // should be betwee
+98
+
+*/
+function buildAnnounceReq(connection_id) {
+    const buffer = Buffer.allocUnsafe(98);
+    connection_id.copy(buffer);
+    buffer.writeUint32BE(1, 8);
+    crypto.randomBytes(4).copy(buffer, 12);
+}
