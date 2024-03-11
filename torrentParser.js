49d92916b2922391f47673a076f52d8d59d12bfd
tomasrobino <tomaserobino@gmail.com>
2024-03-11 01:16:01 +0100

    infoHash(torrent) done

--- torrentParser.js
+++ torrentParser.js
@@ -2,8 +2,19 @@
 
 import fs from "fs";
 import bencode from "bencode";
+import crypto from "crypto";
 
 
 export function open(path) {
     return bencode.decode(fs.readFileSync(path));
+}
+
+
+//Turns torrent's info property into a hash, to be used by the socket
+export function infoHash(torrent) {
+    //Encode into bencode torrent's info property
+    const info = bencode.encode(torrent.info);
+    //Creates hash
+    //Uses SHA1 because it's the algotrithm used by Bittorrent
+    return crypto.createHash("sha1").update(info).digest();
 }
