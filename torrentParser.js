7216ec22c80ad3a5adc1096b1631b1240685c2da
tomasrobino <tomaserobino@gmail.com>
2024-03-11 12:48:04 +0100

    size(torrent) done

--- torrentParser.js
+++ torrentParser.js
@@ -9,7 +9,6 @@ export function open(path) {
     return bencode.decode(fs.readFileSync(path));
 }
 
-
 //Turns torrent's info property into a hash, to be used by the socket
 export function infoHash(torrent) {
     //Encode into bencode torrent's info property
@@ -17,4 +16,15 @@ export function infoHash(torrent) {
     //Creates hash
     //Uses SHA1 because it's the algotrithm used by Bittorrent
     return crypto.createHash("sha1").update(info).digest();
+}
+
+export function size(torrent) {
+    let size;
+    if (torrent.info.files) {
+        size = BigInt(torrent.info.files.map(e => e.length).reduce((acc, val) => acc+val));
+    } else size = BigInt(torrent.info.length);
+
+    const buffer = Buffer.alloc(8);
+    buffer.writeBigInt64BE(size);
+    return buffer;
 }
