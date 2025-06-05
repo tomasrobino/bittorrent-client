3272b9bcd6244640dc0d6e21664608d768da0083
Tomas Robino <tomaserobino@gmail.com>
2025-06-05 16:13:49 +0200

    changed program to work with magnets, commented-out bencode, created magnet_link struct

--- src/main.c
+++ src/main.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct {
+    char* xt; //URN containing file hash
+    char* dn; //A filename to display to the user
+    long xl;  //The file size, in bytes
+    char* tr; //Tracker URL
+    char* ws; //The payload data served over HTTP(S)
+    char* as; //Refers to a direct download from a web server
+    char* xs; //Either an HTTP (or HTTPS, FTP, FTPS, etc.) download source for the file pointed to by the Magnet link,
+              //the address of a P2P source for the file or the address of a hub (in the case of DC++),
+              //by which a client tries to connect directly, asking for the file and/or its sources
+    char* kt; //Specifies a string of search keywords to search for in P2P networks, rather than a particular file
+    char* mt; //Link to the metafile that contains a list of magnets
+    char* so; //Lists specific files torrent clients should download,
+              //indicated as individual or ranges (inclusive) of file indexes.
+    char* xpe;//Specifies fixed peer addresses to connect to
+} magnet_data;
 
 //Check if char is a digit
 bool is_digit(char c);
+//Decode magnet link
+
 
 //Decode bencoded string, returns decoded string
-char* decode_bencode(const char* bencoded_value);
-
+//char* decode_bencode(const char* bencoded_value);
+/*
 int main(const int argc, char* argv[]) {
 	// Disable output buffering
 	setbuf(stdout, nullptr);
@@ -34,13 +52,13 @@ int main(const int argc, char* argv[]) {
     }
     return 0;
 }
-
+*/
 
 bool is_digit(const char c) {
     return c >= '0' && c <= '9';
 }
 
-
+/*
 char* decode_bencode(const char* bencoded_value) {
     // Byte strings
     if (is_digit(bencoded_value[0])) {
@@ -89,4 +107,29 @@ char* decode_bencode(const char* bencoded_value) {
 
     fprintf(stderr, "Unsupported formatting\n");
     exit(1);
+}
+*/
+
+
+int main(const int argc, char* argv[]) {
+    // Disable output buffering
+    setbuf(stdout, nullptr);
+    setbuf(stderr, nullptr);
+
+    if (argc < 3) {
+        fprintf(stderr, "Usage: bittorrent-client.sh <command> <args>\n");
+        return 1;
+    }
+
+    const char* command = argv[1];
+    fprintf(stderr, "Logging will appear here.\n");
+
+    if (strcmp(command, "magnet") == 0) {
+        const char* magnet_link = argv[2];
+
+    } else {
+        fprintf(stderr, "Unknown command: %s\n", command);
+        return 1;
+    }
+    return 0;
 }
