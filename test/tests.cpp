bac6190078cf9868cad702590063b672e7edaf55
Tomas Robino <tomaserobino@gmail.com>
2025-06-19 20:01:24 +0200

    added test for url_to_ip()

--- test/tests.cpp
+++ test/tests.cpp
@@ -11,7 +11,7 @@ TEST_CASE("parse_address detects protocol correctly") {
         address_t* addr = split_address("udp://example.com:1234");
         REQUIRE(addr->protocol == UDP);
         REQUIRE(std::string(addr->host) == "example.com");
-        REQUIRE(addr->port == 1234);
+        REQUIRE(addr->port == "1234");
         free(addr->host);
         free(addr);
     }
@@ -20,7 +20,7 @@ TEST_CASE("parse_address detects protocol correctly") {
         address_t* addr = split_address("https://secure.com:443");
         REQUIRE(addr->protocol == HTTPS);
         REQUIRE(std::string(addr->host) == "secure.com");
-        REQUIRE(addr->port == 443);
+        REQUIRE(addr->port == "443");
         free(addr->host);
         free(addr);
     }
@@ -29,8 +29,22 @@ TEST_CASE("parse_address detects protocol correctly") {
         address_t* addr = split_address("http://plain.com:80");
         REQUIRE(addr->protocol == HTTP);
         REQUIRE(std::string(addr->host) == "plain.com");
-        REQUIRE(addr->port == 80);
+        REQUIRE(addr->port == "80");
         free(addr->host);
         free(addr);
     }
+}
+
+
+TEST_CASE("url_to_ip resolves known hostname", "[network]") {
+    const address_t* address = split_address("udp://tracker.leechers-paradise.org:6969");
+    char* ip = url_to_ip(*address);
+
+    REQUIRE(ip != nullptr);
+
+    REQUIRE(
+        strcmp(ip, "199.59.243.228") == 0
+    );
+
+    free(ip);
 }
