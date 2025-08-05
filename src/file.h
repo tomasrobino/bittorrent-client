a9ef148fafb1de53017705fbe98ac118adf7abbb
Tomas Robino <tomaserobino@gmail.com>
2025-08-05 19:18:12 +0200

    documentation for file.h

--- src/file.h
+++ src/file.h
@@ -3,37 +3,71 @@
 
 #include "util.h"
 
+/**
+ * Represents a linked list node for managing file information in a linked list structure.
+ * Each node contains data about a file and a pointer to the next node in the list.
+ */
 typedef struct files_ll {
     struct files_ll* next;
     unsigned long length;
     ll* path;
 } files_ll;
 
+/**
+ * Represents a nested linked list for managing tracker URLs from announce-list.
+ * Each node contains data related to a list of tracker URLs and a pointer to the next node in the list.
+ */
 typedef struct announce_list_ll {
     struct announce_list_ll* next;
     ll* list;
 } announce_list_ll;
 
+/**
+ * Represents metadata and content information for a torrent file.
+ * Contains details about file(s), piece information, and hash identifiers.
+ *
+ * @field files Linked list of file information for multi-file torrents
+ * @field length Total size of all files in the torrent in bytes
+ * @field name Name of the torrent (single file) or directory name (multiple files)
+ * @field piece_length Size of each piece in bytes
+ * @field piece_number Total number of pieces
+ * @field pieces String containing SHA1 hashes of all pieces concatenated
+ * @field priv Whether the torrent is private (true) or public (false)
+ * @field hash 20-byte SHA1 hash of the info dictionary
+ * @field human_hash 40-character hex string representation of info hash
+ */
 typedef struct {
-    files_ll* files;
+    files_ll *files;
     unsigned long length;
-    char* name;
+    char *name;
     unsigned int piece_length;
     unsigned int piece_number;
-    char* pieces;
+    char *pieces;
     bool priv;
     char hash[21];
     char human_hash[41];
 } info_t;
 
+/**
+ * Represents the complete metadata structure of a torrent file.
+ * Contains primary tracker URL, tracker lists, and other descriptive information.
+ *
+ * @field announce Primary tracker URL that clients use to connect
+ * @field announce_list List of backup tracker URLs organized in tiers
+ * @field comment Optional descriptive comment about the torrent
+ * @field created_by Optional string identifying the program used to create the torrent
+ * @field creation_date Unix timestamp when the torrent was created
+ * @field encoding Optional string specifying character encoding of strings in the torrent
+ * @field info Pointer to structure containing core torrent content information
+ */
 typedef struct {
-    char* announce;
-    announce_list_ll* announce_list;
-    char* comment;
-    char* created_by;
+    char *announce;
+    announce_list_ll *announce_list;
+    char *comment;
+    char *created_by;
     unsigned int creation_date;
-    char* encoding;
-    info_t* info;
+    char *encoding;
+    info_t *info;
 } metainfo_t;
 
 /**
@@ -81,6 +115,4 @@ void free_info_files_list(files_ll *list);
  * @return None. The memory is freed and the pointer becomes invalid.
  */
 void free_metainfo(metainfo_t *metainfo);
-
-
 #endif //FILE_H
