595710a686d2c5a583ce2518625670f98016b9d8
Tomas Robino <tomaserobino@gmail.com>
2025-08-11 11:54:22 +0200

    downloading.h is now doxygen compliant

--- src/downloading.h
+++ src/downloading.h
@@ -1,52 +1,40 @@
 #ifndef DOWNLOADING_H
 #define DOWNLOADING_H
 #include "file.h"
-// Maximum events cached by epoll
+/// @brief Maximum events cached by epoll
 #define MAX_EVENTS 128
-// Maximum amount of time epoll will wait for sockets to be ready (in milliseconds)
+/// @brief Maximum amount of time epoll will wait for sockets to be ready (in milliseconds)
 #define EPOLL_TIMEOUT 5000
-// Download block size in bytes (16KB)
+/// @brief Download block size in bytes (16KB)
 #define BLOCK_SIZE 16384
 
-// Enum for peer statuses
+/// @brief Enum for peer statuses
 typedef enum {
-    PEER_NOTHING, // Untouched peer
-    PEER_CONNECTION_SUCCESS, // Peer connected
-    PEER_CONNECTION_FAILURE, // Peer failed connection
-    PEER_HANDSHAKE_SENT,
-    PEER_HANDSHAKE_SUCCESS,
-    PEER_BITFIELD_RECEIVED,
-    PEER_NO_BITFIELD,
-    PEER_PENDING_WRITE
+    PEER_NOTHING, /**< Untouched peer */
+    PEER_CONNECTION_SUCCESS, /**< Peer connected */
+    PEER_CONNECTION_FAILURE, /**< Peer failed connection */
+    PEER_HANDSHAKE_SENT, /**< Handshake sent to peer */
+    PEER_HANDSHAKE_SUCCESS, /**< Handshake completed successfully */
+    PEER_BITFIELD_RECEIVED, /**< Received bitfield from peer */
+    PEER_NO_BITFIELD, /**< No bitfield received from peer */
+    PEER_PENDING_WRITE /**< Pending write operation to peer */
 } PEER_STATUS;
 
-/**
- * Represents peer data and state in a BitTorrent connection.
- *
- * @field bitfield Bit array representing the pieces this peer has
- * @field id 20-byte string peer ID used during handshake 
- * @field status Current status of the peer connection 
- * @field peer_choked Whether we are choking by the peer
- * @field client_choked Whether we are choked the peer
- * @field peer_interest Whether peer is interested in our pieces
- * @field client_interest Whether we are interested in peer's pieces
- * @field socket Socket file descriptor for this peer connection
- * @field last_msg Timestamp of last message received from peer
- */
+/// @brief Represents peer data and state in a BitTorrent connection
 typedef struct {
-    unsigned char *bitfield;
-    char *id;
-    PEER_STATUS status;
-    bool peer_choked;
-    bool client_choked;
-    bool peer_interest;
-    bool client_interest;
-    int socket;
-    time_t last_msg;
+    unsigned char *bitfield; /**< Bit array representing the pieces this peer has */
+    char *id; /**< 20-byte string peer ID used during handshake */
+    PEER_STATUS status; /**< Current status of the peer connection */
+    bool peer_choked; /**< Whether we are choking by the peer */
+    bool client_choked; /**< Whether we are choked the peer */
+    bool peer_interest; /**< Whether peer is interested in our pieces */
+    bool client_interest; /**< Whether we are interested in peer's pieces */
+    int socket; /**< Socket file descriptor for this peer connection */
+    time_t last_msg; /**< Timestamp of last message received from peer */
 } peer_t;
 
 /**
- * Constructs a full file path as a string from a linked list of directory segments
+ * @brief Constructs a full file path as a string from a linked list of directory segments
  * and creates the necessary directory structure if it does not exist.
  *
  * @param filepath A pointer to a linked list of `ll` structures, where each node
@@ -54,10 +42,10 @@ typedef struct {
  * @return A dynamically allocated string containing the full file path. The caller
  * is responsible for freeing the allocated memory.
  */
-char* get_path(const ll* filepath);
+char *get_path(const ll *filepath);
 
 /**
- * Reads a block of data from a specified socket and writes it into the provided buffer.
+ * @brief Reads a block of data from a specified socket and writes it into the provided buffer.
  *
  * This function continuously reads data from the given socket until the requested
  * amount of data is read or an error occurs.
@@ -67,18 +55,20 @@ char* get_path(const ll* filepath);
  * @param amount The amount of data, in bytes, to read from the socket.
  * @return The total number of bytes successfully read from the socket. Returns -1 in case of an error.
  */
-int32_t read_block_from_socket(int sockfd, unsigned char* buffer, int64_t amount);
+int32_t read_block_from_socket(int sockfd, unsigned char *buffer, int64_t amount);
+
 /**
- * Writes a specified number of bytes from a buffer to a given file.
+ * @brief Writes a specified number of bytes from a buffer to a given file.
  *
  * @param buffer Pointer to the buffer containing the data to be written.
  * @param amount Number of bytes to write to the file.
  * @param file Pointer to the file object where data will be written.
  * @return The number of bytes successfully written, or -1 if an error occurred.
  */
-int32_t write_block(const unsigned char* buffer, int64_t amount, FILE* file);
+int32_t write_block(const unsigned char *buffer, int64_t amount, FILE *file);
+
 /**
- * Downloads a specific block of data from a peer and writes it to the corresponding file(s) based on the metadata.
+ * @brief Downloads a specific block of data from a peer and writes it to the corresponding file(s).
  *
  * @param sockfd The socket file descriptor used for communication with the peer.
  * @param piece_index The index of the piece to which the block belongs.
@@ -88,13 +78,14 @@ int32_t write_block(const unsigned char* buffer, int64_t amount, FILE* file);
  *                       including their lengths and paths. The files must be in order according to their index
  * @return Returns 0 on successful downloading and writing of the block. An error code may otherwise be returned.
  */
-int download_block(int sockfd, unsigned int piece_index, unsigned int piece_size, unsigned int byte_offset, files_ll* files_metainfo);
+int download_block(int sockfd, unsigned int piece_index, unsigned int piece_size, unsigned int byte_offset,
+                   files_ll *files_metainfo);
 
 /**
- * Downloads & uploads torrent
+ * @brief Downloads & uploads torrent
  * @param metainfo The torrent metainfo extracted from the .torrent file
  * @param peer_id The chosen peer_id
- * @returns 0 for success, !0 for failure
-*/
-int torrent(metainfo_t metainfo, const char* peer_id);
+ * @return 0 for success, !0 for failure
+ */
+int torrent(metainfo_t metainfo, const char *peer_id);
 #endif //DOWNLOADING_H
