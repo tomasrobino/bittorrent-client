f92a40d6e66114345b5e5d4c2db4e5f4b3eecbea
Tomas Robino <tomaserobino@gmail.com>
2025-10-23 15:43:48 +0200

    better logging

--- src/downloading.c
+++ src/downloading.c
@@ -578,10 +578,12 @@ int32_t torrent(const metainfo_t metainfo, const unsigned char *peer_id, const L
                 peer->reception_pointer = 0;
                 bittorrent_message_t *message = (bittorrent_message_t *) peer->reception_cache;
                 message->payload = peer->reception_cache + 5;
-                if (log_code == LOG_FULL) fprintf(stdout, "Peer %d received payload:\n", peer->socket);
                 message->payload = peer->reception_cache + MESSAGE_LENGTH_AND_ID_SIZE;
-                for (int k = 0; k < message->length - 1; ++k) {
-                    fprintf(stdout, "%d|", message->payload[k]);
+                if (log_code == LOG_FULL) {
+                    for (int k = 0; k < message->length - 1; ++k) {
+                        fprintf(stdout, "%d|", message->payload[k]);
+                    }
+                    fprintf(stdout, "Peer %d received payload:\n", peer->socket);
                 }
 
                 switch (message->id) {
